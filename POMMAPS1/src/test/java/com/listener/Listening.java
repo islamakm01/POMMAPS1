c09f70f694f43fb5208596f4d78e95c2ce99d17a
islama2 <akim.islam@medtronic.com>
2024-11-11 22:57:25 -0600

    Update Extent Test Manager Reports

--- POMMAPS1/src/test/java/com/listener/Listening.java
+++ POMMAPS1/src/test/java/com/listener/Listening.java
@@ -1,5 +1,8 @@
 package com.listener;
 
+import Utility.ExtentManager;
+import Utility.ExtentTestManager;
+import com.aventstack.extentreports.Status;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -9,21 +12,29 @@ public class Listening implements ITestListener {
     public void onTestStart(ITestResult result) {
         // not implemented
         System.out.println("Running the Test Method Name :- " + result.getMethod().getMethodName());
+
+        ExtentTestManager.startTest(result.getMethod().getMethodName());
     }
 
     public void onTestSuccess(ITestResult result) {
         // not implemented
         System.out.println("Running the Test Method Name :- " + result.getMethod().getMethodName() + "Test is pass");
+
+        ExtentTestManager.getTest().log(Status.PASS,"Test Passed");
     }
 
     public void onTestFailure(ITestResult result) {
         // not implemented
         System.out.println("Running the Test Method Name :- " + result.getMethod().getMethodName() + "Test is failed");
+
+        ExtentTestManager.getTest().log(Status.FAIL,"Test Failed");
     }
 
     public void onTestSkipped(ITestResult result) {
         // not implemented
         System.out.println("Running the Test Method Name :- " + result.getMethod().getMethodName() + "Test is Skipped");
+
+        ExtentTestManager.getTest().log(Status.SKIP,"Test Skipped");
     }
 
     public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
@@ -43,6 +54,9 @@ public class Listening implements ITestListener {
         // not implemented
 
         System.out.println("Test Suite Ended and will remove all memory " + context.getName());
+
+        ExtentTestManager.endTest();
+        ExtentManager.getInstance().flush();
     }
 
 }
